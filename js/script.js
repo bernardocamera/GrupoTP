ba92539f2787766183901dac818108804f9998b1
Claudio <claudiomdangelo@gmail.com>
2023-10-25 19:56:17 -0300

    Version Entrega

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+const str = "contacto.html";
+if( window.location.href.includes(str))
+{
+console.log ("estas adentro")
 const nombre = document.getElementById("nombre")
 const apellido = document.getElementById("apellido")
 const dni = document.getElementById("dni")
@@ -71,5 +75,57 @@ form.addEventListener("submit" , e=>{
             parrafo.innerHTML=warnings
     }  
 
-})
+})}
+
+const str1 = "clima.html";
+if( window.location.href.includes(str1))
+
+{
+console.log("Estas en clima")
+const btnLeft = document.querySelector('.btn-left'),
+btnRight = document.querySelector('.btn-right'),
+slider = document.querySelector('#slider'),
+sliderSection = document.querySelectorAll('.slider-section');
+
+console.log(sliderSection);
+
+btnLeft.addEventListener('click', e => moveToLeft());
+btnRight.addEventListener('click', e => moveToRight());
+
+let operation = 0;
+let widthImg = 100 / sliderSection.length;
+let counter = 0;
+
+function moveToRight(){
+    if(counter >= sliderSection.length - 1){
+        counter = 0;
+        operation = 0;
+        slider.style.transform = `translate(-${operation}%)`;  
+        slider.style.transition = 'none';  
+    } else {
+        counter ++;
+        operation = operation + widthImg;
+        slider.style.transform = `translate(-${operation}%)`;
+        slider.style.transition = 'none';
+    }
+}
+
+setInterval(() => {
+    moveToLeft();
+}, 3000);
+
+function moveToLeft(){
+    counter--;
+    if(counter < 0){
+        counter = sliderSection.length - 1;
+        operation = widthImg * (sliderSection.length - 1);
+        slider.style.transform = `translate(-${operation}%)`;
+        slider.style.transition = 'none';
+    } else {
+
+        operation = operation - widthImg;
+        slider.style.transform = `translate(-${operation}%)`;
+        slider.style.transition = 'all ease .6s';
+    }
+}}
 
